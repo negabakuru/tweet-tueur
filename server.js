2d85168d3871e60253d9d51c8ff640a88cd53d6b
Yaël Beaufort <y.beaufort@student.isartdigital.com>
2016-10-17 19:42:28 +0200

    Server reads config file and stores the values for later. Added a package.json with the application's dependencies.

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 var http = require('http');
 var fs = require('fs');
 
+var twitter = require('twitter');
+var twitterInfos;
+fs.readFile('twitter_infos.json', 'utf8', function (error, data) {
+    if (error)
+        return console.log(error);
+    twitterInfos = JSON.parse(data);
+    console.log('Data from twitter_infos.json: ' + twitterInfos.hashtag + ' ' + twitterInfos.consumer_key + ' ' + twitterInfos.consumer_secret + ' ' + twitterInfos.access_token_key + ' ' + twitterInfos.access_token_secret);
+});
+
+
 function handleRequest(request, response)
 {
-    console.log("Server received a request");
+    console.log("Server received a request.");
 
     fs.readFile('index.html', 'utf8', function (error, data) {
         if (error)
@@ -17,3 +27,4 @@ const port = 4242;
 server.listen(port, function () {
     console.log("Server listening on port " + port)
 });
+
